e81cca6db8c3fad6d6e73dc13e9c57c16c223dde
Daniel Verlaque <daniel.verlaque@gmail.com>
2018-04-19 19:12:28 -0400

    basic curl test

--- testing/flaskTests.sh
+++ testing/flaskTests.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+
+failedTests=0
+
+
+TEST_NAME="top-level get"
 curlOutput=$(curl -X GET 137.112.89.91:5005 -s)
 echo -e ">> ${curlOutput}"
 correct="hello"
 if [ "$curlOutput" = "$correct" ] ; then
-	echo -e "top-level get passed"
+	echo -e "${GREEN}${TEST_NAME} passed ${NC}"
 else
-	echo -e "top-level get failed; should be $correct"
-	exit 1
+	echo -e "${RED}${TEST_NAME} failed; should be ${NC} $correct"
+	failedTests=1
 fi
+
+
+exit $failedTests
