2fc403610020d4fb7c6c34949da3ef94c5185b74
Daniel Verlaque <daniel.verlaque@gmail.com>
2018-04-19 19:14:41 -0400

    deliberately failing test

--- testing/flaskTests.sh
+++ testing/flaskTests.sh
@@ -19,4 +19,16 @@ else
 fi
 
 
+TEST_NAME="deliberately failing test"
+curlOutput=$(curl -X GET 137.112.89.9:5005 -s --connect-timeout 5)
+echo -e ">> ${curlOutput}"
+correct="hello"
+if [ "$curlOutput" = "$correct" ] ; then
+	echo -e "${GREEN}${TEST_NAME} passed ${NC}"
+else
+	echo -e "${RED}${TEST_NAME} failed; should be ${NC} $correct"
+	failedTests=1
+fi
+
+
 exit $failedTests
