ff767ca203bd653a5d096012b5120bd8a2409b73
Daniel Verlaque <daniel.verlaque@gmail.com>
2018-04-25 16:22:31 -0400

    get all courses

--- backend/db/courses.py
+++ backend/db/courses.py
@@ -18,6 +18,10 @@ def course_main(courseId):
 
 
 def courseGet(courseId):
+    if not courseId:
+        # return all courseId
+        return Response(response=json.dumps(list(course_bucket.n1ql_query('select * from courses'))), status=200, mimetype='application/json')
+
     cb_data = course_bucket.get(courseId)  # type: ValueResult
     return Response(response=json.dumps(cb_data.value), status=200, mimetype='application/json')
 
