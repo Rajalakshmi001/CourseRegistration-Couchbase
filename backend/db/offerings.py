7ec21003237ec3100578c4805b03a39628cc6dad
Daniel Verlaque <daniel.verlaque@gmail.com>
2018-04-25 17:06:18 -0400

    offerings

--- backend/db/offerings.py
+++ backend/db/offerings.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Response, json, make_response
 from db.couchbase_server import *
 import couchbase.subdocument as subdoc 
+from couchbase.result import SubdocResult
 from utils import catch404, require_json_data, catch_already_exists
 
 offering_bucket = cluster.open_bucket('offerings')
@@ -17,19 +18,24 @@ def offering_main(quarterId, courseId, sectionId):
 
 
 def offeringGet(quarterId, courseId, sectionId):
-    ob_data = offering_bucket.lookup_in(quarterId, subdoc.get(courseId+"."+sectionId))
-    return Response(response=json.dumps(ob_data.value), status=200, mimetype='application/json')
+    ob_data = offering_bucket.lookup_in(quarterId, subdoc.get(courseId+("."+sectionId if int(sectionId) else "")))  # type: SubdocResult
+    
+    return Response(response=json.dumps(list(ob_data)[0]), status=200, mimetype='application/json')
 
 
-@catch_already_exists
 @require_json_data
 def offeringPut(quarterId, courseId, sectionId):
+    @catch_already_exists
+    def mquarter():
+        offering_bucket.insert(quarterId, {})
+    mquarter()
+
     offering_bucket.mutate_in(quarterId, subdoc.insert(courseId+"."+sectionId, request.get_json(), create_parents=True))
     return make_response("Created offering {}/{}-{}".format(quarterId, courseId, sectionId), 200)
 
 
 def offeringPost(quarterId, courseId, sectionId):
-    offering_bucket.mutate_in(quarterId, subdoc.insert(courseId+"."+sectionId, request.get_json()))
+    offering_bucket.mutate_in(quarterId, subdoc.replace(courseId+"."+sectionId, request.get_json()))
     return make_response("Updated offering {}/{}-{}".format(quarterId, courseId, sectionId), 200)
 
 
