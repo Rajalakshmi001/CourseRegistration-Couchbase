ab9a7d9c43a2fd96ddc768c605d58c9ae5c59b2b
Daniel Verlaque <daniel.verlaque@gmail.com>
2018-04-26 16:10:40 -0400

    lots of offering stuff

--- backend/db/offerings.py
+++ backend/db/offerings.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, Response, json, make_response
 from db.couchbase_server import *
-import couchbase.subdocument as subdoc 
 from couchbase.result import SubdocResult
 from utils import catch404, require_json_data, catch_already_exists
-from db.quarters import quarterPut as upsert_quarter
+from db.quarters import quarterPut as upsert_quarter, quarterGet as get_for_quarter
 
 offering_bucket = cluster.open_bucket('offerings')
 
@@ -19,16 +18,37 @@ def offering_main(quarterId, courseId, sectionId):
 
 
 def offeringGet(quarterId, courseId, sectionId):
+    if not sectionId:
+        if not courseId:
+            if not quarterId:
+                return all_offerings()
+            return get_for_quarter(quarterId)
+        return get_course_sections(quarterId, courseId)
+    return get_single_offering(quarterId, courseId, sectionId)
+    
     ob_data = offering_bucket.lookup_in(quarterId, subdoc.get(courseId+("" if not sectionId else '.'+sectionId)))  # type: SubdocResult
     return Response(response=json.dumps(list(ob_data)[0]), status=200, mimetype='application/json')
+
+def all_offerings():
+    # can I really do that? 
+    return None
+
+def __offering_lookup_helper(qId, path):
+    ob_data = offering_bucket.lookup_in(qId, subdoc.get(path))  # type: SubdocResult
+    return Response(response=json.dumps(list(ob_data)[0]), status=200, mimetype='application/json')
+
+def get_course_sections(quarterId, courseId):
+    return __offering_lookup_helper(quarterId, courseId)
+
+def get_single_offering(quarterId, courseId, sectionId):
+    return __offering_lookup_helper(quarterId, courseId+'.'+sectionId)
 
 
 @require_json_data
 def offeringPut(quarterId, courseId, sectionId):
     upsert_quarter(quarterId)
-
     offering_bucket.mutate_in(quarterId, subdoc.insert(courseId+"."+sectionId, request.get_json(), create_parents=True))
-    return make_response("Created offering {}/{}-{}".format(quarterId, courseId, sectionId), 200)
+    return make_response("Created offering {}/{}-{}".format(quarterId, courseId, sectionId), 201)
 
 
 def offeringPost(quarterId, courseId, sectionId):
@@ -37,4 +57,6 @@ def offeringPost(quarterId, courseId, sectionId):
 
 
 def offeringDelete(quarterId, courseId, sectionId):
-    pass
+    sec = ("." + sectionId) if sectionId else ''
+    offering_bucket.mutate_in(quarterId, subdoc.remove(courseId + sec))
+    return make_response("Deleted", 204)
