5b00a0a8b3e5eff2382c4626d7f64139678ccb0c
Daniel Verlaque <daniel.verlaque@gmail.com>
2018-04-26 17:32:39 -0400

    advanced Python tests

---
+++ testing/backendTests.py
@@ -0,0 +1,36 @@
+from test_methods import test_all
+
+
+def test_users():
+    print(">>> Testing users")
+    test_user_path = '/user/my_test_user'
+    all_user_path = '/user'
+    test_user_data = dict(
+            username='my_test_user', 
+            name='test user')
+
+    test_all(test_user_data, test_user_path, all_user_path)
+    print("\n>> Done testing users\n\n")
+
+
+def test_courses():
+    print(">>> Testing courses")
+    test_course_path = '/course/TEST123'
+    all_courses_path = '/course'
+    test_course_data = dict(courseNum='TEST123', sectionNum="01", description='this is a test course', name='Test Course')
+    test_all(test_course_data, test_course_path, all_courses_path)
+    print("\n>> Done testing courses\n\n")
+
+
+def test_offerings():
+    print(">>> Testing offerings")
+    test_offering_data = dict(professor="TEST_PROF", sectionId="01", courseId="OtestCourse", quarterId="Summer2000")
+    test_offering_path = "/offering/Summer2000/OTestCourse/01"
+    others = [["/offering/Summer2000/OTestCourse/02", 
+                dict(professor="SECOND_TEST_PROF", sectionId="02", courseId="OtestCourse", quarterId="Summer2000")]]
+    test_all(test_offering_data, test_offering_path, "/offering/Summer2000/OTestCourse", others)
+
+
+test_users()
+test_courses()
+test_offerings()
---
+++ testing/test_methods.py
@@ -0,0 +1,83 @@
+import requests
+import json
+from requests import Response
+url = "http://137.112.89.91:5005"  # "http://localhost:5005" 
+uri = lambda x: url+x
+
+
+def delete(path):
+    print("--------------- DELETE")
+    r = requests.delete(path)
+    print(r.status_code, r.text)
+    return r.status_code
+
+
+def put(path, data):
+    print("---------------- PUT")
+    r = requests.put(path, json=data)  # type: Response
+    print(r.status_code, r.text)
+    return r.status_code
+
+
+def get(path):
+    print("---------------- GET")
+    r = requests.get(path)  # type: Response
+    returned = r.json() if r.content else None
+    print(r.status_code, " GET returned:", returned)
+    return returned
+
+
+def get_all(path):
+    print("---------------- GET ALL")
+    r = requests.get(path)  # type: Response
+    returned = r.json() if r.content else None
+    print("> GET ALL returned:", returned)
+    return returned
+
+
+def post(path, data):
+    print("---------------- POST")
+    r = requests.post(path, data)
+    return r.status_code
+
+
+def test_all(sample_data: dict, sample_path: str, all_path=None, others_for_all=None, post_changes=None):
+    sample_path = uri(sample_path)
+    # delete
+    delete(sample_path)
+    # get should return null
+    assert not get(sample_path)
+    # put
+    assert 200 <= put(sample_path, sample_data) < 304
+    # get should return data
+    assert get(sample_path) == sample_data
+
+    if all_path:
+        all_path = uri(all_path)
+        if others_for_all:
+            for other_path, other_data in others_for_all:
+                assert 200 <= put(uri(other_path), other_data) <= 304
+        else:
+            others_for_all = []
+        # get all should contain sample
+        all_res = get_all(all_path)
+        assert sample_data in all_res
+        for _,other in others_for_all:
+            assert other in all_res
+    else:
+        print("Not testing 'get all' functionality because path was not given")
+    
+    if post_changes:
+        updated = {k:v for k,v in list(sample_data)}
+        for k in post_changes:
+            updated[k] = post_changes[k]
+        post(sample_path, updated)
+
+        assert get(sample_path) == updated
+    else:
+        print("not testing post")
+
+    # delete again
+    delete(sample_path)
+    # and check for None again
+    assert not get(sample_path)
