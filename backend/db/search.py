5ed233b8dafd7f3010f0c3e1823e1b5b59eb7b11
Daniel Verlaque <daniel.verlaque@gmail.com>
2018-05-03 17:49:28 -0400

    search should work

--- backend/db/search.py
+++ backend/db/search.py
@@ -13,14 +13,18 @@ redis_client = Redis(
 
 
 def run_search(queryString=None, department=None):
-    keywords = list(word.lower() for word in queryString.split(" ") if len(word) > 3)
+    
+    if queryString:
+        keywords = list(word.lower() for word in queryString.split(" ") if len(word) > 3)
+        keyword_indices = sorted('ind:'+keyword for keyword in keywords)
+        sus = "search_{}".format(randint(0,1000000))
+        print(keyword_indices)
+        if department:
+            redis_client.sunionstore(sus, keyword_indices)
+            return list(redis_client.sinter(("dept:"+department, sus)))
 
-    keyword_indices = sorted('ind:'+keyword for keyword in keywords)
-    sus = "search_{}".format(randint(0,1000000))
-    if department:
-        redis_client.sunionstore(sus, keyword_indices)
-        return redis_client.sinter(("dept:"+department, sus))
+        return list(redis_client.sunion(keyword_indices))
 
-    return redis_client.sunion(keyword_indices)
+    # if department:
 
     
